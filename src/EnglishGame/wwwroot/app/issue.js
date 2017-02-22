5c1ee319192b92e924e880a4ee36ce0ac9498772
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-02-22 20:26:34 +0300

    angular app

--- src/EnglishGame/wwwroot/app/issue.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Issue = (function () {
-    function Issue() {
-    }
-    return Issue;
-}());
-exports.Issue = Issue;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXNzdWUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9hcHAvaXNzdWUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBO0lBQUE7SUFHQSxDQUFDO0lBQUQsWUFBQztBQUFELENBQUMsQUFIRCxJQUdDO0FBSFksc0JBQUsiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgSXNzdWUge1xyXG4gICAgaWQ6IG51bWJlcjtcclxuICAgIG5hbWU6IHN0cmluZztcclxufSJdfQ==
