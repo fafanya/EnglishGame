d8e939cf94a59818bb98878e61de68bf983b4b88
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-02-27 16:57:19 +0300

    new signalr

--- src/EnglishGame/typings/globals/jasmine/index.d.ts
+++ src/EnglishGame/typings/globals/jasmine/index.d.ts
@@ -29,7 +29,7 @@ interface DoneFn extends Function {
     fail: (message?: Error|string) => void;
 }
 
-//declare function spyOn<T>(object: T, method: keyof T): jasmine.Spy;
+declare function spyOn<T>(object: T, method: keyof T): jasmine.Spy;
 
 declare function runs(asyncMethod: Function): void;
 declare function waitsFor(latchMethod: () => boolean, failureMessage?: string, timeout?: number): void;
