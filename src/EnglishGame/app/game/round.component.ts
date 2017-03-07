2a74265491164e8e5ea4f433c6254d940ad25e16
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-03-07 13:19:21 +0300

    some actions

--- src/EnglishGame/app/game/round.component.ts
+++ src/EnglishGame/app/game/round.component.ts
@@ -5,6 +5,20 @@
     templateUrl: '../html/game/round.component.html'
 })
 export class RoundComponent implements OnInit {
-    ngOnInit(): void {
+
+    question = "1+6";
+    leftVariant = "2";
+    rightVariant = "7";
+ 
+    ngOnInit(): void
+    {
+    }
+
+    leftChoice() {
+        var b = 2 + 2;
+    }
+
+    rightChoice() {
+        var a = 1 + 1;
     }
 }
