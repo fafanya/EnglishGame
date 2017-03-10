bd2eb5e081ec065673b34479179b68ddda39a938
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-03-10 08:44:44 +0300

    game.service

--- src/EnglishGame/app/game/round.component.ts
+++ src/EnglishGame/app/game/round.component.ts
@@ -1,15 +1,15 @@
 ﻿import { Component, OnInit } from '@angular/core';
-//import { GameService } from './game.service';
+import { GameService } from './game.service';
 
 @Component({
     selector: 'round',
     templateUrl: '../html/game/round.component.html',
-    //providers: [GameService]
+    providers: [GameService]
 })
 export class RoundComponent implements OnInit {
 
     question = "1+6";
-    leftVariant = "2";
+    leftVariant = "4";
     rightVariant = "7";
  
     ngOnInit(): void
