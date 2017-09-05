52b705ad483c1915fab604333bb00938d64a0c47
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-09-05 16:01:29 +0300

    workbook initial commit

---
+++ src/EnglishGame/Models/Workbook/UMark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishGame.Models.Workbook
+{
+    public class UMark
+    {
+        public int Mark;
+        public bool IsPlus;
+        public bool IsMinus;
+    }
+}
