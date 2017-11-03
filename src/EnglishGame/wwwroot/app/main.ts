17009da64db5a565c95ed6226dfb7d196f5f7d70
eugene afanasovich <eugeneafanasovich@gmail.com>
2017-11-03 11:44:51 +0300

    pupil page

--- src/EnglishGame/wwwroot/app/main.ts
+++ src/EnglishGame/wwwroot/app/main.ts
@@ -1,4 +1,4 @@
 ﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './game/app.module';
+import { AppModule } from './workbook/app.module';
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
--- src/EnglishGame/wwwroot/app/workbook/pupil-area.component.ts
+++ src/EnglishGame/wwwroot/app/workbook/pupil-area.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UExercise } from './models/uexercise';
 
 @Component({
     moduleId: module.id,
@@ -10,6 +11,13 @@ export class PupilAreaComponent implements OnInit {
     isLogin = false;
 
     private exerciseAmount: number;
+
+    exercises: UExercise[] = [
+        {
+            id: 1, name: 'Exercise 1', teacher_id: 1, taskList: null,
+            pupilList: null, scheduleItemList: null, teacher: null
+        }
+    ];
 
     constructor(
         private router: Router
